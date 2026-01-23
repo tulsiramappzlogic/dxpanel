679a835ac82a05967a9ff10b1884dbfd0f468b86
lalitpal16 <lalit.pal@appzlogic.com>
2026-01-23 18:57:26 +0530

    Updated email accounts in email configuration for all regions

--- UK/email_config.php
+++ UK/email_config.php
@@ -11,11 +11,11 @@ define('SMTP_ENCRYPTION', 'tls');
 define('SMTP_AUTH', true);
 
 // Gmail credentials
-define('SMTP_USERNAME', 'tanmaypatidar09@gmail.com');
-define('SMTP_PASSWORD', 'kivxvtisvtobjojh'); // 16-character app password
+define('SMTP_USERNAME', 'panel.ukpolls@gmail.com');
+define('SMTP_PASSWORD', 'DXpanels08@#$'); // 16-character app password
 
 // Email settings
-define('FROM_EMAIL', 'tanmaypatidar09@gmail.com');
+define('FROM_EMAIL', 'panel.ukpolls@gmail.com');
 define('FROM_NAME', 'UKPolls');
 define('EMAIL_SUBJECT', 'Your OTP for UKPolls Registration');
 
