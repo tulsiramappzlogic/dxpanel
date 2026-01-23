2331a9174005a4016ad93a2e9986a3e3764b0a23
Tulsiram Patidar <tulsiram.patidar@appzlogic.com>
2026-01-23 17:46:50 +0530

    Update input event handling to onblur for form fields across multiple regions

--- PH/index.php
+++ PH/index.php
@@ -131,11 +131,11 @@
               <div class="row g-3">
                 <div class="col-md-4">
                   <input type="text" name="full_name" id="full_name" class="form-control" placeholder="Full Name"
-                    pattern="[a-zA-Z\s]+" oninput="this.value = this.value.replace(/[^a-zA-Z\s]/g, ''); checkFormCompletion();" required />
+                    pattern="[a-zA-Z\s]+" onblur="this.value = this.value.replace(/[^a-zA-Z\s]/g, ''); checkFormCompletion();" required />
                 </div>
                 <div class="col-md-4">
                 <input type="email" name="email" id="email" class="form-control" placeholder="Email"
-                    pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}" oninput="this.value = this.value.replace(/[^a-zA-Z0-9._%+-@]/g, ''); checkFormCompletion();" required />
+                    pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}" onblur="this.value = this.value.replace(/[^a-zA-Z0-9._%+-@]/g, ''); checkFormCompletion();" required />
                 </div>
                 <div class="col-md-4">
                   <input type="date" name="date_of_birth" id="date_of_birth" class="form-control"
@@ -151,22 +151,22 @@
                 </div>
                 <div class="col-md-4">
                   <textarea name="address" id="address" class="form-control" placeholder="Address line" required
-                    rows="1" oninput="checkFormCompletion();"></textarea>
+                    rows="1" onblur="checkFormCompletion();"></textarea>
                 </div>
                 <div class="col-md-4">
                 <input type="text" name="barangay" id="barangay" class="form-control" placeholder="Barangay"
-                    pattern="[a-zA-Z\s]+" oninput="this.value = this.value.replace(/[^a-zA-Z\s]/g, ''); checkFormCompletion();" required />
+                    pattern="[a-zA-Z\s]+" onblur="this.value = this.value.replace(/[^a-zA-Z\s]/g, ''); checkFormCompletion();" required />
                 </div>
                 <div class="col-md-4">
                 <input type="text" name="city" id="city" class="form-control" placeholder="City / Municipality"
-                    pattern="[a-zA-Z\s]+" oninput="this.value = this.value.replace(/[^a-zA-Z\s]/g, ''); checkFormCompletion();" required />
+                    pattern="[a-zA-Z\s]+" onblur="this.value = this.value.replace(/[^a-zA-Z\s]/g, ''); checkFormCompletion();" required />
                 </div>
                 <div class="col-md-4">
                 <input type="text" name="country" id="country" readonly value="Philippines" class="form-control" placeholder="Province"
-                    pattern="[a-zA-Z\s]+" oninput="this.value = this.value.replace(/[^a-zA-Z\s]/g, ''); checkFormCompletion();" required />
+                    pattern="[a-zA-Z\s]+" onblur="this.value = this.value.replace(/[^a-zA-Z\s]/g, ''); checkFormCompletion();" required />
                 </div>
                 <div class="col-md-4">
-                  <input type="text" name="postcode" id="postcode" class="form-control" placeholder="Postcode" oninput="checkFormCompletion();" required />
+                  <input type="text" name="postcode" id="postcode" class="form-control" placeholder="Postcode" onblur="checkFormCompletion();" required />
                 </div>
               </div>            
           </form>
