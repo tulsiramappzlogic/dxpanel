69350f67dae1c5d6b3ef967bcb4285f86dcb4ae8
lalitpal16 <lalit.pal@appzlogic.com>
2026-01-24 11:47:48 +0530

    Added footer to phillippines polls

--- PH/index.php
+++ PH/index.php
@@ -213,7 +213,38 @@
       </div>
     </div>
   </main>
-
+  <footer class="background-grey-1">
+    <div class="container-fluid px-5 py-3">
+      <div class="row gap-3 gap-lg-0 align-items-center">
+        <div class="col-12 col-lg-4 col-xxl-5">
+          <div class="text-center text-lg-start">
+            <a href="#" class="border-right-except-last footer-link px-2">About Us</a>
+            <a href="#" class="border-right-except-last footer-link px-2">Privacy Policy</a>
+            <a href="#" class="border-right-except-last footer-link px-2">Terms &
+              Conditions</a>
+            <a href="#"
+              class="border-right-except-last footer-link px-2">For
+              Clients</a>
+            <a href="contact-us.html" class="border-right-except-last footer-link px-2">Contact Us</a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-4 col-xxl-2">
+          <div class="social-icons">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+          </div>
+        </div>
+        <div class="col-12 col-lg-4 col-xxl-5">
+          <div class="text-center text-lg-end">
+            <p style="font-size: 14px;" class="text-light">
+              @2026 PhillippinesPolls. All Rights Reserved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
   <script src="./script/script.js"></script>
 
