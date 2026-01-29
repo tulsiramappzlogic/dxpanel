618437e0a2cf4ae652ca38822eca315dc7a983d2
lalitpal16 <lalit.pal@appzlogic.com>
2026-01-30 01:39:00 +0530

    Added about us page for phillippines region

---
+++ PH/about-us.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>About Us - PhilippinesPolls</title>
+    <link rel="icon" href="./image/favicon.png" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Expletus+Sans:ital,wght@0,400..700;1,400..700&family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="./style/commonStyle.css" />
+    <link rel="stylesheet" href="./style/index.css" />
+</head>
+
+<body>
+    <?php
+    include 'header.php';
+    ?>
+    <main>
+        <div id="about-us-container" class="container my-5">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-center">About PhilippinesPolls</h1>
+                </div>
+            </div>
+            <div class="row mt-3 align-items-center">
+                <div class="col-md-2">
+                    <img class="w-100" src="./image/about-us-images/about-us-image-1.svg" alt="about us image 1" />
+                </div>
+                <div class="col-md-10">
+                    <div class="heading-container mb-3">
+                        <h2>How does PhilippinesPolls work?</h2>
+                        <img src="./image/icons/curveIcon-grey.png" />
+                    </div>
+                    <section>
+                        <p>It’s simple! You <strong>become a member</strong> and fill in some basic information about yourself, your interests, etc. You start earning i-Points at this time. Each i-Point is worth real rewards.</p>
+                        <p>Then you receive surveys, which you respond to. As a reward for your time, you get i-Points.</p>
+                        <p>You also earn i-Points for <strong>referring friends!</strong></p>
+                        <p>We strongly encourage you to participate in our weekly “<strong>Screensense Audience Pulse</strong>” for a chance to win upto ₱3000 each week!</p>
+                        <p>You can exchange i-Points for <strong>gift vouchers</strong> at popular retailers and brands.</p>
+                    </section>
+                </div>
+            </div>
+            <div class="row mt-3 align-items-center">
+                <div class="col-md-2">
+                    <img class="w-100" src="./image/about-us-images/about-us-image-2.svg" alt="about us image 2" />
+                </div>
+                <div class="col-md-10">
+                    <div class="heading-container  mb-3">
+                        <h2>Earn money - what else?</h2>
+                        <img src="./image/icons/curveIcon-grey.png" />
+                    </div>
+                    <section>
+                        <p>Your replies help real <strong>companies</strong> make real decisions about products and services that affect all of us.</p>
+                        <p><strong>Academic</strong> institutions need to know what you think, for the research they conduct.</p>
+                        <p><strong>Government</strong> and public sector organisations use insights like yours while shaping new policies.</p>
+                        <p>At PhilippinesPolls, we want to understand what people think - because <strong>What the Philippines Thinks matters.</strong></p>
+                    </section>
+                </div>
+            </div>
+            <div class="row mt-3 align-items-center">
+                <div class="col-md-2">
+                    <img class="w-100" src="./image/about-us-images/about-us-image-3.svg" alt="about us image 3" />
+                </div>
+                <div class="col-md-10">
+                    <div class="heading-container  mb-3">
+                        <h2>Who are we?</h2>
+                        <img src="./image/icons/curveIcon-grey.png" />
+                    </div>
+                    <section>
+                        <p>We are Philippines’ growing multi-demographic online community, specially created to collect opinions from people across regions, age groups, income and occupations.</p>
+                        <p>We are a team of market research, technology and business professionals who have come together to build the best platform for online research that Malaysia has ever experienced.</p>
+                        <p>We help make connections between businesses and consumers, so they can make better decisions.</p>
+                        <p>And, we are fully "Made for Philippines" - a division of DataXing Digital India Pvt Ltd.</p>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </main>
+    <?php
+    include 'footer.php';
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="./script/script.js"></script>
+
+</body>
+
+</html>
