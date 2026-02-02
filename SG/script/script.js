7416273ec0b95acb0d7a8797b6081fa40e14601a
Tulsiram Patidar <tulsiram.patidar@appzlogic.com>
2026-02-02 11:20:45 +0530

    basic code comment update

--- SG/script/script.js
+++ SG/script/script.js
@@ -66,7 +66,7 @@ $(document).ready(function () {
   // Make validateEmail globally available
   window.validateEmail = validateEmail;
 
-// Address Validation - Min 15 characters
+// Address Validation - Min 5 characters
   function validateAddress() {
     var address = $("#address").val().trim();
     var minLength = 5;
